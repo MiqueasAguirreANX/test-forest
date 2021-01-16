82cbd1e056a6b42ea816d8c530da910b5bfebee7
MiqueasAguirreANX <70060960+MiqueasAguirreANX@users.noreply.github.com>
2021-01-16 09:12:29 -0300

    adding question view and url

--- test_forest_sigma/core/urls.py
+++ test_forest_sigma/core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from core.views import index
+from core.views import index, question
 app_name = 'core'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('question/<id>', question, name="question")
 ]
+
--- test_forest_sigma/core/views.py
+++ test_forest_sigma/core/views.py
@@ -7,3 +7,8 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     return render(request, "account/base.html")
+
+def question(request, id):
+    # fetch data from database for the index given
+    context = {'id': id }
+    return render(request, 'account/question.html', context)
